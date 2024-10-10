9b787f89c0154f7cc74b475b1c612384b51cecad
Luxyz <elkin.diaz@epn.edu.ec>
2024-10-09 21:39:45 -0500

    commit 1

--- src/Persona.java
+++ src/Persona.java
@@ -17,6 +17,7 @@ public class Persona {
         System.out.println("Hola mi nombre es " + nombre + " y tengo " + edad + "años.");
     }
 
+    //Main
     public static void main(String[] args) {
         Persona p1 = new Persona("Elkin",20,"Masculino");
         Persona p2 = new Persona("Wendy",22,"Femenino");
